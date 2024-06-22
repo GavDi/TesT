5f2270fff5abc80db2a7bd6c6cbe71b6ca6ec7fc
Гаврилов Димитрий <gavrilov.master@gmail.com>
2024-06-22 12:50:59 +0300

    добавил библиотеку хилт

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -51,6 +53,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+
     implementation(libs.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
